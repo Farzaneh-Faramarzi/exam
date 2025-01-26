8580bdf6fec7ba7ccd440363f5b0f2e196198943
Farzaneh <farzanehfaramarzi555@gmail.com>
2025-01-26 00:57:50 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+/*let h1Elem = document.getElementById("title")
+
+ let liElems = document.getElementsByClassName("list-item")
+
+ let listItems = document.getElementsByTagName("li")
+
+let firstListItem = document.querySelector("list-item")
+
+let ulItems = document.querySelectorAll('.list-item')
+
+
+ console.log(ulItems);*/
+ /*___________________________________________________________________________________________
+ ______________________________________________________________________________________________*/
+ /*let h1Elem = document.getElementById('title')
+
+ //console.log(h1Elem.getAttribute('align'));
+
+ //console.log(h1Elem.getAttribute('class'));
+
+ h1Elem.setAttribute('class','js-text')
+
+ let liElems = document.getElementsByClassName('list-item')
+
+ console.log(liElems[0].setAttribute('id','about'));*/
+ /*___________________________________________________________________________________________
+ _____________________________________________________________________________________________*/
+ /*let h1Elem = document.getElementById("title")
+
+ h1Elem.style.color = 'red'
+ h1Elem.style.fontSize = '60px'
+
+ let listItems = document.getElementsByClassName('list-item')
+
+ //console.log(listItems);
+ listItems[3].style.color = 'blue'
+  listItems[3].style.fontWeight = 'bold'
+
+  console.log(listItems);*/
+  /*___________________________________________________________________________________________________
+  _____________________________________________________________________________________________________*/
+  
